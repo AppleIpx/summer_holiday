f5bf9c6f82d81470352a5b46726d3317f20040a7
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-11-19 22:19:11 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Сайт загружен!');
+  // Пример: приветственное сообщение
+  alert('Добро пожаловать на сайт о летнем отдыхе!');
+});
